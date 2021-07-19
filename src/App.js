9e594601da2d62f64a7d1a27ed154130a1072614
João Stael <joaostael@gmail.com>
2021-07-19 20:31:32 -0300

    Página de cadastro e header iniciados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Cadastro from './pages/Cadastro';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/">
           <Cadastro />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,11 @@ import './style.css';
 
 function Header() {
   return (
-    <header>
-      <p>Bem vindo,</p>
+    <header className="p-3 mb-2 bg-primary text-white">
+       
+            <p><em>Bem vindo,</em> Fulano</p>
+            <i className="fas fa-user-circle iconeUsuario"></i>
+      
     </header>
   )
 }
